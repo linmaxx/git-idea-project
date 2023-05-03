c37b77c911740845d187a16e7b366a116875e9b8
linmaxxx <1634221587@qq.com>
2023-05-03 18:26:03 +0800

    更新1

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: lin
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试试 github");
+    }
+}
